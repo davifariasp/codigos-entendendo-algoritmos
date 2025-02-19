f8ffe5e07f98729a0b6633c24cce37e4e0748f2a
Davi <davi.informatica8@gmail.com>
2025-02-19 11:37:59 -0300

    feature: algoritmo quicksort

--- src/main/java/codigos/quicksort/Quicksort.java
+++ src/main/java/codigos/quicksort/Quicksort.java
@@ -1,4 +1,32 @@
 package codigos.quicksort;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Quicksort {
+    //lista esquerda e direita iniciam vazias
+    //pivo vai ser escolhido aleatoriamente
+    public static List<Integer> ordenar(List<Integer> lista) {
+        if(lista.size() <2){
+            return lista;
+        } else {
+            //indice aleatorio
+            Random random = new Random();
+            int indiceAleatorio = random.nextInt(lista.size());
+
+            Integer pivo = lista.get(indiceAleatorio);
+
+            List<Integer> listaEsquerda = lista.stream().filter(x -> x < pivo).toList();
+            List<Integer> listaDireita = lista.stream().filter(x -> x > pivo).toList();
+
+            List<Integer> resultado = new ArrayList<>();
+
+            resultado.addAll(ordenar(listaEsquerda));
+            resultado.add(pivo);
+            resultado.addAll(ordenar(listaDireita));
+
+            return resultado;
+        }
+    }
 }
