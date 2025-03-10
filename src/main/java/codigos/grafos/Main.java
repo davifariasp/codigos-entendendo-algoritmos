491b33ac14e216029b081a993dd6941d17488f26
Davi <davi.informatica8@gmail.com>
2025-03-10 11:05:02 -0300

    feature: grafos

---
+++ src/main/java/codigos/grafos/Main.java
@@ -0,0 +1,28 @@
+package codigos.grafos;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Map<String, Integer>> lista = new LinkedList<>();
+
+        Map<String, Integer> verticeInicio = new HashMap<>(){{
+            put("A", 6);
+            put("B", 2);
+        }};
+
+        Map<String, Integer> verticeA = new HashMap<>(){{
+            put("C", 1);
+        }};
+
+        Map<String, Integer> verticeB = new HashMap<>(){{
+            put("A", 3);
+            put("C", 5);
+        }};
+
+        Map<String, Integer> verticeFim = new HashMap<>();
+
+
+    }
+}
